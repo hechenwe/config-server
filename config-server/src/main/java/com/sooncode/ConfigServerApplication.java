3f8cc2927ca37baa0065bbb458c34a315fd80b98
he chen <he chen@DESKTOP-O38CH3R>
2018-01-01 22:57:25 +0800

    hechen

--- config-server/src/main/java/com/sooncode/ConfigServerApplication.java
+++ config-server/src/main/java/com/sooncode/ConfigServerApplication.java
@@ -1,12 +1,15 @@
 package com.sooncode;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
 @EnableConfigServer
+ 
+@SpringBootConfiguration  
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
